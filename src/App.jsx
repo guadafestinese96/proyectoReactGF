7a7dd99d4f90587edb68fae63156e36e3187c122
guadafestinese96 <guadalupefestinese@gmail.com>
2024-04-30 20:20:59 -0300

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
     <ItemListContainer greating="Hola WidiFollowers"/>
      <Routes>
       <Route exact path="/" element={<h1>Home</h1>} />
-       <Route exact path="/fraganciasHombre" element={<h1>Perfumes para hombre</h1>} />
+       <Route exact path="/fraganciasHombre" element={<h1>Perfumes para Hombre</h1>} />
+       <Route exact path="/fraganciasMujer" element={<h1>Perfumes para Mujer</h1>} />
        <Route exact path="/perfumes/*" element={<h1>Not found</h1>} />
     </Routes>
     
