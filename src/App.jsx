e79d3a5a2dcb5108092be176cb2bdc46f6d76e0d
guadafestinese96 <guadalupefestinese@gmail.com>
2024-05-01 20:14:42 -0300

    ordeno perfumes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import Layout from './components/Layout/Layout'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function App() {
   return (
--- src/components/mock/asyncMock.js
+++ src/components/mock/asyncMock.js
@@ -1,84 +1,84 @@
 const perfumes = [
-{
-    id: 1,
-    marca: "Paco Rabanne",
-    nombre: "Lady Million",
-    precio: 26000,
-    genero: "Femenino",
-    stock: 2,
-    image: "../src/Perfumes/ladyMillion.jpg"
-},
-{
-    id: 2,
-    marca: "Paco Rabanne",
-    nombre: "Black XS L'Excess",
-    genero: "Femenino",
-    precio: 26000,
-    stock: 1,
-    image: "../src/Perfumes/blackXsLexcessM.jpg"
-},
-{
-    id: 3,
-    marca: "Paco Rabanne",
-    nombre: "One Million",
-    precio: 26000,
-    genero: "Masculino",
-    stock: 8,
-    image: "../src/Perfumes/oneMillion.jpg"
-},
-{
-    id: 4,
-    marca: "Paco Rabanne",
-    nombre: "Black XS L'excess",
-    genero: "Masculino",
-    precio: 26000,
-    stock: 5,
-    image: "../src/Perfumes/blackXsLexcessH.jpg"
-},
-{
-    id: 5,
-    marca: "Paco Rabanne",
-    nombre: "Pure XS",
-    genero: "Masculino",
-    precio: 26000,
-    stock: 2,
-    image: "../src/Perfumes/pureXsH.jpg"
-},
-{
-    id: 6,
-    marca: "Paco Rabanne",
-    nombre: "Pure XS",
-    genero: "Femenino",
-    precio: 26000,
-    stock: 1,
-    image: "../src/Perfumes/pureXsM.jpg"
-},
-{
-    id: 7,
-    marca: "Paco Rabanne",
-    nombre: "Black XS",
-    genero: "Masculino",
-    precio: 26000,
-    stock: 5,
-    image: "../src/Perfumes/blackXsH.jpg"
-},
-{
-    id: 8,
-    marca: "Paco Rabanne",
-    nombre: "Black XS",
-    genero: "Femenino",
-    precio: 26000,
-    stock: 5,
-    image: "../src/Perfumes/blackXsM.jpg"
-}
+    {
+        id: 1,
+        marca: "Paco Rabanne",
+        nombre: "One Million",
+        precio: 26000,
+        genero: "Masculino",
+        stock: 8,
+        image: "../src/Perfumes/oneMillion.jpg"
+    },
+    {
+        id: 2,
+        marca: "Paco Rabanne",
+        nombre: "Lady Million",
+        precio: 26000,
+        genero: "Femenino",
+        stock: 2,
+        image: "../src/Perfumes/ladyMillion.jpg"
+    },
+    {
+        id: 3,
+        marca: "Paco Rabanne",
+        nombre: "Black XS L'excess",
+        genero: "Masculino",
+        precio: 26000,
+        stock: 5,
+        image: "../src/Perfumes/blackXsLexcessH.jpg"
+    },
+    {
+        id: 4,
+        marca: "Paco Rabanne",
+        nombre: "Black XS L'Excess",
+        genero: "Femenino",
+        precio: 26000,
+        stock: 1,
+        image: "../src/Perfumes/blackXsLexcessM.jpg"
+    },
+    {
+        id: 5,
+        marca: "Paco Rabanne",
+        nombre: "Pure XS",
+        genero: "Masculino",
+        precio: 26000,
+        stock: 2,
+        image: "../src/Perfumes/pureXsH.jpg"
+    },
+    {
+        id: 6,
+        marca: "Paco Rabanne",
+        nombre: "Pure XS",
+        genero: "Femenino",
+        precio: 26000,
+        stock: 1,
+        image: "../src/Perfumes/pureXsM.jpg"
+    },
+    {
+        id: 7,
+        marca: "Paco Rabanne",
+        nombre: "Black XS",
+        genero: "Masculino",
+        precio: 26000,
+        stock: 5,
+        image: "../src/Perfumes/blackXsH.jpg"
+    },
+    {
+        id: 8,
+        marca: "Paco Rabanne",
+        nombre: "Black XS",
+        genero: "Femenino",
+        precio: 26000,
+        stock: 5,
+        image: "../src/Perfumes/blackXsM.jpg"
+    }
 ]
 
-export const getPerfumes = () =>{
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
+export const getPerfumes = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
             resolve(perfumes);
             console.log(perfumes);
         }, 2000);
     })
-    
+
 }
