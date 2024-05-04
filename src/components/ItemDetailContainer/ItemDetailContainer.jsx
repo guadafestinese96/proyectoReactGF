8339a7c9eeb5f89eccb6879d4178a44fd561e46e
guadafestinese96 <guadalupefestinese@gmail.com>
2024-05-04 18:12:16 -0300

    ---

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,16 +1,17 @@
 import usePerfume from "../../hooks/usePerfume";
 import Item from "../ItemList/Item";
 
-export default function ItemDetailContainer(idParams){
-    const {perfume, isLoading} = usePerfume(idParams);
+export default function ItemDetailContainer(id){
+    id=4;
+    const {perfume, isLoading} = usePerfume(id);
+    console.log(perfume);
 
     if (isLoading) return <h1>Cargando...</h1>
 
     return(
         <div>
             <h1>item</h1>
-            <h1>{perfume.nombre}</h1>
-           
+            <Item item={perfume}/>
         </div>    
         )
 }
