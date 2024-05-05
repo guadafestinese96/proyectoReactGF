fabade20abe0e9ee21bfb73e4ba7414ff3bf020c
guadafestinese96 <guadalupefestinese@gmail.com>
2024-05-05 17:13:01 -0300

    Widi

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 
 
 export default function ItemDetailContainer(){
+
     const {idParams} = useParams();
     const {perfume, isLoading} = usePerfume(idParams);
 
