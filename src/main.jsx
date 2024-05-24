7e61bdf668aedc7309012f15e5bbdfc023e040e3
guadafestinese96 <guadalupefestinese@gmail.com>
2024-05-24 00:15:22 -0300

    agrego firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4Uslc0a9zg37lpKuAu4uW_-JUfwMVcx0",
+  authDomain: "widistorefbreact.firebaseapp.com",
+  projectId: "widistorefbreact",
+  storageBucket: "widistorefbreact.appspot.com",
+  messagingSenderId: "415690217605",
+  appId: "1:415690217605:web:b2d692d9b743d78d0271ec"
+};
+
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />,
